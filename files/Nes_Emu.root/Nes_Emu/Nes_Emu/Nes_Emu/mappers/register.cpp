ceb7dbd9ff2a7d7f5daaf5aa41612f8e3deab59c
Chris Moeller <kode54@gmail.com>
2010-01-11 08:58:47 +0000

    {10/19/2005 12:06:24 AM~10/19/2005 12:08:00 AM}
    git-tfs-id: [http://localhost:8080/tfs/DefaultCollection]$/emulation;C30

---
+++ files/Nes_Emu.root/Nes_Emu/Nes_Emu/Nes_Emu/mappers/register.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+
+void register_mmc5_mapper();
+void register_vrc6_mapper();
+void register_misc_mappers();
+void register_fme07_mapper();
+
+void register_mappers();
+void register_mappers()
+{
+	register_mmc5_mapper();
+	register_vrc6_mapper();
+	register_misc_mappers();
+	register_fme07_mapper();
+}
